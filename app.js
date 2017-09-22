36ca027e730505dad6f85258b246211984f1c93a
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-09-22 22:07:45 +0200

    add redis as cache

--- app.js
+++ app.js
@@ -1,63 +1,96 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var Redis = require('ioredis');
+var redis = new Redis(6379, 'redis');
 
-app.use(bodyParser.text({type: '*/*', limit: Infinity}));
-var cache = {};
+app.use(bodyParser.text({ type: '*/*', limit: Infinity }));
 
-app.put('/:key', function(req, res) {
+app.put('/:key', function (req, res) {
 
     var key = req.params.key;
 
-    if(typeof cache[key] !== 'undefined' && cache[key].locked === true){
-        return res.sendStatus(423)
-    }
+    redis.get(key).then(function (value) {
 
-    var data = {
-        "data": req.body,
-        "locked": false
-    }
+        value = JSON.parse(value);
 
-    cache[key] = data;
+        if(value === null){
+            value = { locked: false, data: "" }
+        }
 
-    return res.send("bashcache.com/"+key+'\n');
+        if (value.locked === true) {
+            return res.sendStatus(423)
+        }
+
+        value.data = req.body;
+
+        redis.set(key, JSON.stringify(value));
+
+        return res.send("bashcache.com/" + key + '\n');
+
+    }).catch(function (error) {
+        res.sendStatus(500);
+    });
 
 });
 
-app.lock('/:key', function(req, res) {
+app.lock('/:key', function (req, res) {
 
-    if(typeof cache[req.params.key] === 'undefined'){
-        return res.sendStatus(404);
-    }
-
-    if(cache[req.params.key].locked === true){
-        return res.sendStatus(423);
-    }
-
-    cache[req.params.key].locked = true;
-
-    res.sendStatus(200);
-});
-
-app.get('/:key', function(req, res) {
-    
     var key = req.params.key;
 
-    var data = cache[key].data
+    redis.get(key).then(function (value) {
 
-    if (typeof data === 'undefined'){
-        return res.sendStatus(404);
-    }
+        value = JSON.parse(value);
 
-    res.set("Content-type", "text/html");
+        if(value === null){
+            return res.sendStatus(404);
+        }
 
-    return res.send(data+'\n');
-    
+        if (value.locked === true) {
+            return res.sendStatus(423)
+        }
+
+        value.locked = true;
+
+        redis.set(key, JSON.stringify(value));
+
+        return res.sendStatus(200);
+
+    }).catch(function () {
+        res.sendStatus(500);
+    });
+
 });
 
-app.get('/', function(req, res){
+app.get('/:key', function (req, res) {
+
+    var key = req.params.key;
+
+    redis.get(key).then(function (value) {
+
+        value = JSON.parse(value);
+
+        if(value === null){
+            return res.sendStatus(404);
+        }
+
+        if (value.locked === true) {
+            return res.sendStatus(423)
+        }
+
+        res.set("Content-type", "text/html");
+
+        return res.send(value.data + '\n');
+
+    }).catch(function () {
+        res.sendStatus(500);
+    });
+
+});
+
+app.get('/', function (req, res) {
     return res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
